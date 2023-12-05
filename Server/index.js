82666cbe8c0cab2e90ad3dd49ae2b505e1128c9a
Kevin9921 <kevinjohnsonn@live.ca>
2023-12-05 04:22:46 -0500

    Server access db and transfers to client

--- Server/index.js
+++ Server/index.js
@@ -1,8 +1,31 @@
 const express = require('express')
 const app = express()
+let mysql = require('mysql');
+
+let charData
+
+let connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'one Piece'
+});
+
+connection.connect(function(err) {
+    if (err) {
+      return console.error('error: ' + err.message);
+    }
+    connection.query("SELECT * FROM onepiece_characters", 
+        function (err, result) {
+            if (err) throw err;
+            charData = result
+            console.log("yo",charData);
+        });
+    console.log('Connected to the MySQL server.');
+  });
 
 app.get("/api", (req, res) => {
-    res.json({"users": ["usersOne", "userTwo", "userThreee"]})
+    res.json(charData)
 })
 
 app.listen(5000, () => {console.log("Server started on port 5000")})
